208c6a8bc6d36eb3d63be016e659f3a27e5c9adc
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-19 14:22:17 +0600

    sord

--- src/Components/ReadList/ReadList.jsx
+++ src/Components/ReadList/ReadList.jsx
@@ -21,8 +21,33 @@ useEffect(() => {
     setReatList(myList);
 }, []);
 
+const [sort,setSort]=useState("");
+
+const handleSortedBtn=(type)=>{
+setSort(type);
+if(type==="pages"){
+    const sordByPages=[...readList].sort((a,b)=>a.totalPages-b.totalPages)
+    setReatList(sordByPages);
+}
+
+if(type==="rating"){
+    const sortByRating=[...readList].sort((a,b)=>a.rating-b.rating);
+    setReatList(sortByRating)
+}
+
+}
+
   return (
     <div className="w-10/12 mx-auto py-8">
+    <div className="py-10 flex justify-center items-center">
+    <details className="dropdown">
+  <summary className="btn m-1 bg-[#23BE0A] text-white">Sort By : {sort? sort:""}</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a onClick={()=>handleSortedBtn("pages")}>Pages</a></li>
+    <li><a onClick={()=>handleSortedBtn("rating")}>Rating</a></li>
+  </ul>
+</details>
+    </div>
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
--- src/Components/ReadListDetails/ReadListDetails.jsx
+++ src/Components/ReadListDetails/ReadListDetails.jsx
@@ -5,6 +5,7 @@ const ReadListDetails = ({singleReadLIst}) => {
     const {bookName,author,image,totalPages,publisher,rating,yearOfPublishing,review,category,tags}=singleReadLIst;
     return (
         <div className="max-w-4xl mx-auto p-4">
+        
   <div className="bg-white rounded-xl shadow flex gap-4 p-6 items-center">
     {/* Book Image */}
     <div className="w-32 flex-shrink-0">
