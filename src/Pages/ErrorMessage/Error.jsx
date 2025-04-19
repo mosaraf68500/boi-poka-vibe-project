08e371f5c6933518f59a4b710cfce27181b2e04b
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-20 00:47:25 +0600

    error page work

--- src/Pages/ErrorMessage/Error.jsx
+++ src/Pages/ErrorMessage/Error.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import image from "../../assets/error.jpg"
+import { NavLink } from 'react-router';
 
 const Error = () => {
     return (
-        <div>
-            <h1>page not found:404!</h1>
+        <div className='flex space-y-4 flex-col  justify-center items-center  mt-10'>
+            <img className='py-10 w-[400px] border shadow-2xl rounded-2xl border-gray-300' src={image} alt="" />
+            <NavLink to="/"><button className='btn py-4 btn-success text-white font-bold'>Back To Home</button></NavLink>
         </div>
     );
 };
